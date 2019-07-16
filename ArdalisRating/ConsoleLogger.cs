ec19f4af10e366f5cc9532f39cce5235034619c2
Scott <sneidig@gmail.com>
2019-07-15 22:26:05 -0700

    Add ConsoleLogger class

---
+++ ArdalisRating/ConsoleLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ArdalisRating
+{
+    public class ConsoleLogger
+    {
+   
+        public Log(string logger)
+        {
+            Console.Writeline(logger);
+            
+        }
+    }
+}
