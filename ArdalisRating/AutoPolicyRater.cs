d8652115dc8df607cefe261fdc56c66c39622dd0
Scott <sneidig@gmail.com>
2019-07-15 21:48:09 -0700

    Moved contents of switch statement for auto policy in RatingEngine class to AutoPolicyRater class

---
+++ ArdalisRating/AutoPolicyRater.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArdalisRating
+{
+    class AutoPolicyRater
+    {
+
+        private readonly RatingEngine _engine;
+        private ConsoleLogger _logger;
+
+        public AutoPolicyRater(RatingEngine engine, ConsoleLogger logger)
+        {
+            _engine = engine;
+            _logger = logger;
+        }
+
+        public void Rate(Policy policy)
+        {
+            _logger.Log("Rating AUTO policy...");
+            _logger.Log("Validating policy.");
+            if (String.IsNullOrEmpty(policy.Make))
+            {
+                _logger.Log("Auto policy must specify Make");
+                return;
+            }
+            if (policy.Make == "BMW")
+            {
+                if (policy.Deductible < 500)
+                {
+                    Rating = 1000m;
+                }
+                Rating = 900m;
+            }
+        }
+    }
+}
