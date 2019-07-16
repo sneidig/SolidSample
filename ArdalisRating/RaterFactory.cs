9f82215f06319e7436fc65a54088ee214d3a72e8
Scott <sneidig@gmail.com>
2019-07-15 22:49:18 -0700

    Add RaterFactory class

---
+++ ArdalisRating/RaterFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArdalisRating
+{
+    class RaterFactory
+    {
+        public Rater Create(Policy policy, RatingEngine engine)
+        {
+            switch (policy.Type)
+            {
+                case PolicyType.Auto:
+                    return new AutoPolicyRater(engine, engine.Logger);
+                case PolicyType.Land:
+                    return new LandPolicyRater(engine, engine.Logger);
+                case PolicyType.Life:
+                    return new LifePolicyRater(engine, engine.Logger);
+
+                default:
+                    // Null Object Pattern
+
+            }
+        }
+    }
+}
