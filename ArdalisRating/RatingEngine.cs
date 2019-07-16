175bf33014f2b84d32c517b51a38ba863a7562e8
Scott <sneidig@gmail.com>
2019-07-15 22:33:37 -0700

    Couple switch statement to *PolicyRater classes

--- ArdalisRating/RatingEngine.cs
+++ ArdalisRating/RatingEngine.cs
@@ -12,6 +12,8 @@ namespace ArdalisRating
     public class RatingEngine
     {
         public decimal Rating { get; set; }
+        public ConsoleLogger Logger { get; set; }
+
         public void Rate()
         {
             Console.WriteLine("Starting rate.");
@@ -27,79 +29,26 @@ namespace ArdalisRating
             switch (policy.Type)
             {
                 case PolicyType.Auto:
-                    Console.WriteLine("Rating AUTO policy...");
-                    Console.WriteLine("Validating policy.");
-                    if (String.IsNullOrEmpty(policy.Make))
-                    {
-                        Console.WriteLine("Auto policy must specify Make");
-                        return;
-                    }
-                    if (policy.Make == "BMW")
-                    {
-                        if (policy.Deductible < 500)
-                        {
-                            Rating = 1000m;
-                        }
-                        Rating = 900m;
-                    }
+                    var rater = new AutoPolicyRater(this, this.Logger);
+                    rater.Rate(policy);
                     break;
 
                 case PolicyType.Land:
-                    Console.WriteLine("Rating LAND policy...");
-                    Console.WriteLine("Validating policy.");
-                    if (policy.BondAmount == 0 || policy.Valuation == 0)
-                    {
-                        Console.WriteLine("Land policy must specify Bond Amount and Valuation.");
-                        return;
-                    }
-                    if (policy.BondAmount < 0.8m * policy.Valuation)
-                    {
-                        Console.WriteLine("Insufficient bond amount.");
-                        return;
-                    }
-                    Rating = policy.BondAmount * 0.05m;
-                    break;
+                    var rater2 = new LandPolicyRater(this, this.Logger);
+                    rater2.Rate(policy);
+                    break;                    
 
                 case PolicyType.Life:
-                    Console.WriteLine("Rating LIFE policy...");
-                    Console.WriteLine("Validating policy.");
-                    if (policy.DateOfBirth == DateTime.MinValue)
-                    {
-                        Console.WriteLine("Life policy must include Date of Birth.");
-                        return;
-                    }
-                    if (policy.DateOfBirth < DateTime.Today.AddYears(-100))
-                    {
-                        Console.WriteLine("Centenarians are not eligible for coverage.");
-                        return;
-                    }
-                    if (policy.Amount == 0)
-                    {
-                        Console.WriteLine("Life policy must include an Amount.");
-                        return;
-                    }
-                    int age = DateTime.Today.Year - policy.DateOfBirth.Year;
-                    if (policy.DateOfBirth.Month == DateTime.Today.Month &&
-                        DateTime.Today.Day < policy.DateOfBirth.Day ||
-                        DateTime.Today.Month < policy.DateOfBirth.Month)
-                    {
-                        age--;
-                    }
-                    decimal baseRate = policy.Amount * age / 200;
-                    if (policy.IsSmoker)
-                    {
-                        Rating = baseRate * 2;
-                        break;
-                    }
-                    Rating = baseRate;
+                    var rater3 = new LifePolicyRater(this, this.Logger);
+                    rater3.Rate(policy);
                     break;
 
                 default:
-                    Console.WriteLine("Unknown policy type");
+                    Logger.Log("Unknown policy type");
                     break;
             }
-
-            Console.WriteLine("Rating completed.");
+            
+            Logger.Log("Rating completed.");
         }
     }
 }
